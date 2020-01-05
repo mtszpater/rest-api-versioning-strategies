3d65fb7b07a64a5ab9f81b846188ba506af8181f
Mateusz Pater <mtszpater@gmail.com>
2020-01-05 14:39:22 +0100

    change domain

--- src/main/java/dev/pater/app/Controller.java
+++ src/main/java/dev/pater/app/Controller.java
@@ -1,18 +1,27 @@
 package dev.pater.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping(value = "/", produces = "application/json;version=1")
-    public String version1() {
-        return "1.0";
+    private final PostService postService;
+
+    @Autowired
+    public Controller(PostService postService) {
+        this.postService = postService;
     }
 
-    @GetMapping(value = "/", produces = "application/json;version=2")
-    public String version2() {
-        return "2.0";
+    @GetMapping(value = "posts/{id}", produces = "application/json;version=1")
+    public String post1(@PathVariable int id) {
+        return postService.getOne(id, "1.0");
+    }
+
+    @GetMapping(value = "posts/{id}", produces = "application/json;version=2")
+    public String post2(@PathVariable int id) {
+        return postService.getOne(id, "2.0");
     }
 }
